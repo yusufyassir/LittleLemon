78e28dea6ee414a0b1c52c635215e33dc06d23b4
yusufyassir <yusufyassir0@gmail.com>
2025-01-17 13:45:02 +0200

    final hopefully

--- config/settings.py
+++ config/settings.py
@@ -141,7 +141,7 @@ REST_FRAMEWORK = {
         'rest_framework.filters.OrderingFilter',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 1,  # Number of items per page
+    'PAGE_SIZE': 4,  # Number of items per page
 }
 
 DJOSER = {
